7512a4a8c95436a9d8c0fb8098f239a1d60069a0
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-03-13 23:07:53 -0400

    more stuff

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from rply import ParserGenerator, LexerGenerator
 from rply.token import BaseBox
 
 lg = LexerGenerator()
-lg.add('IDENT', r'^[_a-zA-Z]\w*')
+
 lg.add('ADD', r'\+')
 lg.add('SUB', r'\-')
 lg.add('DIV', r'\\')
@@ -12,11 +12,22 @@ lg.add('INT', r'\d+')
 lg.add('LPAREN', r'\(')
 lg.add('RPAREN', r'\)')
 
-lg.add('COLON2', r'\.')
+lg.add('IMPORT', r'import')
+lg.add('TYPE', r'type')
+lg.add('VARIANT', r'variant')
+lg.add('ENUM', r'enum')
+lg.add('UNION', r'union')
+
+lg.add('COLON2', r'::')
+
+lg.add('IDENT', r'[_a-zA-Z]([_a-zA-Z0-9]*)')
 
 lg.ignore(r'\s+')
 
-pg = ParserGenerator(['INT', 'ADD', 'SUB', 'MUL', 'DIV', 'MOD', 'LPAREN', 'RPAREN', 'IDENT', 'COLON2'],
+pg = ParserGenerator([
+    'INT', 'ADD', 'SUB', 'MUL', 'DIV', 'MOD', 
+    'LPAREN', 'RPAREN', 'IDENT', 'COLON2', 'IMPORT',
+    'TYPE', 'VARIANT', 'ENUM', 'UNION'],
     precedence=[
         ('left', ['ADD', 'SUB']),
         ('left', ['MUL', 'DIV', 'MOD'])
@@ -35,10 +46,74 @@ class Import:
     def __init__(self, path):
         self.path = path
 
-@pg.production('main : path')
-def main(p):
-    return p[0]
+class Func:
+    def __init__(self, decl):
+        self.decl = decl
 
+class Tuple:
+    def __init__(self, fields):
+        self.fields = fields
+
+class Struct:
+    def __init__(self, fields):
+        self.fields = fields
+
+class Variant:
+    def __init__(self, fields):
+        self.fields = fields
+
+class Main:
+    def __init__(self, imports = None, body = None):
+        self.imports = imports
+        self.body = body
+
+        for decl in imports:
+            print('import ' + '.'.join(decl.path))
+
+# toplevel
+
+@pg.production('main : import-decls')
+def main(p):
+    return Main(imports = p[0])
+
+@pg.production('main : body-decls')
+def main1(p):
+    return Main(body = p[0])
+
+@pg.production('main : import-decls body-decls')
+def main2(p):
+    return Main(imports = p[0], body = p[1])
+
+# body
+
+@pg.production('body-decls : body-decl')
+def body(p):
+    return [p[0]]
+
+@pg.production('body-decls : body-decl body-decls')
+def body1(p):
+    return p[0] + p[1]
+
+@pg.production('body-decl : LPAREN RPAREN')
+def body2(p):
+    return None
+
+# imports
+
+@pg.production('import-decls : import-decl')
+def import_decls(p):
+    return [p[0]]
+
+@pg.production('import-decls : import-decl import-decls')
+def import_decls2(p):
+    return [p[0]] + p[1]
+
+@pg.production('import-decl : IMPORT path')
+def import_decl(p):
+    return Import([str(part.getstr()) for part in p[1]])
+
+
+# dotted-name
 @pg.production('path : IDENT')
 def path(p):
     return [p[0]]
@@ -46,6 +121,8 @@ def path(p):
 @pg.production('path : IDENT COLON2 path')
 def path2(p):
     return [p[0]] + p[2]
+
+
 
 @pg.production('expr : expr ADD expr')
 @pg.production('expr : expr SUB expr')
@@ -83,4 +160,7 @@ def error(token):
 lex = lg.build()
 parse = pg.build()
 
-print(parse.parse(lex.lex('ab . a')))
+ast = parse.parse(lex.lex('''
+import java::util::HashMap
+import x::y::z
+'''))
