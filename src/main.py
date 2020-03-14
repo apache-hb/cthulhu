2a0439e0379cca75527f57680831c9a453b276b5
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-03-14 01:42:27 -0400

    more ops and exprs

--- src/main.py
+++ src/main.py
@@ -8,9 +8,46 @@ lg.add('SUB', r'\-')
 lg.add('DIV', r'\\')
 lg.add('MUL', r'\*')
 lg.add('MOD', r'\%')
-lg.add('INT', r'\d+')
+
+lg.add('ADDEQ', r'\+=')
+lg.add('SUBEQ', r'\-=')
+lg.add('DIVEQ', r'\\=')
+lg.add('MULEQ', r'\*=')
+lg.add('MODEQ', r'\%=')
+
+lg.add('SHL', r'<<')
+lg.add('SHR', r'>>')
+lg.add('SHLEQ', r'<<=')
+lg.add('SHREQ', r'>>=')
+
+lg.add('BITNOT', r'\~')
+
+lg.add('BITAND', r'\&')
+lg.add('BITANDEQ', r'\&=')
+
+lg.add('BITOR', r'\|')
+lg.add('BITOREQ', r'\|=')
+
+lg.add('ASSIGN', r':=')
+lg.add('EQ', r'==')
+lg.add('NEQ', r'\!=')
+lg.add('NOT', r'\!')
+
+lg.add('ARROW', r'\->')
+lg.add('BIGARROW', r'=>')
+
+lg.add('HEX', r'0x[0-9a-fA-F]+')
+lg.add('BIN', r'0b[0-1]+')
+lg.add('NUM', r'[\d.]+')
+
 lg.add('LPAREN', r'\(')
 lg.add('RPAREN', r'\)')
+
+lg.add('LSQUARE', r'\[')
+lg.add('RSQUARE', r'\]')
+
+lg.add('LBRACE', r'\{')
+lg.add('RBRACE', r'\}')
 
 lg.add('IMPORT', r'import')
 lg.add('TYPE', r'type')
@@ -25,7 +62,7 @@ lg.add('IDENT', r'[_a-zA-Z]([_a-zA-Z0-9]*)')
 lg.ignore(r'\s+')
 
 pg = ParserGenerator([
-    'INT', 'ADD', 'SUB', 'MUL', 'DIV', 'MOD', 
+    'NUM', 'HEX', 'BIN', 'ADD', 'SUB', 'MUL', 'DIV', 'MOD', 
     'LPAREN', 'RPAREN', 'IDENT', 'COLON2', 'IMPORT',
     'TYPE', 'VARIANT', 'ENUM', 'UNION'],
     precedence=[
@@ -50,6 +87,7 @@ class Func:
     def __init__(self, decl):
         self.decl = decl
 
+
 class Tuple:
     def __init__(self, fields):
         self.fields = fields
@@ -62,13 +100,14 @@ class Variant:
     def __init__(self, fields):
         self.fields = fields
 
+class Enum:
+    def __init__(self, fields):
+        self.fields = fields
+
 class Main:
     def __init__(self, imports = None, body = None):
         self.imports = imports
         self.body = body
-
-        for decl in imports:
-            print('import ' + '.'.join(decl.path))
 
 # toplevel
 
@@ -149,9 +188,17 @@ def expr_op(p):
 def paren_expr(p):
     return p[1]
 
-@pg.production('expr : INT')
-def expr_int(p):
-    return BoxInt(int(p[0].getstr()))
+@pg.production('expr : NUM')
+def num_expr(p):
+    return p[0].getstr()
+
+@pg.production('expr : BIN')
+def bin_expr(p):
+    return p[0].getstr()[2:]
+
+@pg.production('expr : HEX')
+def hex_expr(p):
+    return p[0].getstr()[2:]
 
 @pg.error
 def error(token):
