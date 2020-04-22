99f94b9d330c6df121b6a8294d72613503cdc5dc
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-04-22 00:26:16 -0400

    Update types.fs

--- tests/types.fs
+++ tests/types.fs
@@ -8,3 +8,28 @@ type s := { a: int, b: float }
 type t := (int, int)
 
 type u := union { x: int, y: (int, int) }
+
+type i := int
+type j := u32
+type b := b32
+
+type VkBool32 := u32
+
+def VK_TRUE := VkBool32(1)
+def VK_FALSE := VkBool32(0)
+
+
+type VkResult := enum: i32 {
+    Success := 0,
+    NotReady := 1,
+    Timeout := 2,
+    EventSet := 3,
+    EventReset := 4,
+    Incomplete := 4,
+
+    ErrorOutOfHostMemory := -1,
+    ErrorOutOfDeviceMemory := -2,
+    ErrorInitializationFailed := -3,
+    ErrorDeviceList := -4,
+    ErrorMemoryMapFailed := -5
+}
