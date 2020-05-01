083643c9ae66a98e0bc53824c80386fc26f62a89
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-05-01 01:51:49 -0400

    try writing it in c++
    that jai guy did it so its probably not a bad idea

--- src/writer.h
+++ src/writer.h
@@ -12,19 +12,76 @@ void PrintImport(Writer* ctx, Node* node)
 {
     TEXPECT(node, NodeTypeImport);
 
-    PUT(ctx, "import");
+    PUT(ctx, "#include \"");
     
     for(size_t i = 0; i < node->data._import.path.size; i++)
     {
         if(i != 0)
-            PUT(ctx, ":");
+            PUT(ctx, "/");
 
         WRITE(ctx, "%s", node->data._import.path.arr[i]);
     }
 
+    PUT(ctx, ".h\"");
+
     if(node->data._import.alias)
     {
-        WRITE(ctx, " -> %s", node->data._import.alias);
+        WRITE(ctx, " // as %s", node->data._import.alias);
+    }
+
+    PUT(ctx, "\n");
+}
+
+void PrintType(Writer* ctx, Node* node)
+{
+    (void)ctx;
+    (void)node;
+
+    if(node->type == NodeTypeStruct)
+    {
+        
+    }
+    else if(node->type == NodeTypeTuple)
+    {
+
+    }
+    else if(node->type == NodeTypeArray)
+    {
+
+    }
+    else if(node->type == NodeTypeEnum)
+    {
+
+    }
+    else if(node->type == NodeTypeVariant)
+    {
+
+    }
+    else if(node->type == NodeTypeName)
+    {
+
+    }
+    else if(node->type == NodeTypeUnion)
+    {
+
+    }
+}
+
+void PrintTypedef(Writer* ctx, Node* node)
+{
+    TEXPECT(node, NodeTypeTypedef);
+
+    PrintType(ctx, node->data._typedef.type);
+}
+
+void PrintBody(Writer* ctx, Node* node)
+{
+    if(!node)
+        PUT(ctx, "oh no");
+        
+    if(node->type == NodeTypeTypedef)
+    {
+        PrintTypedef(ctx, node);
     }
 }
 
@@ -44,5 +101,10 @@ void PrintProgram(Writer* ctx, Node* node)
     for(size_t i = 0; i < node->data._program.imports.size; i++)
     {
         PrintImport(ctx, node->data._program.imports.arr[i]);
+    }
+
+    for(size_t i = 0; i < node->data._program.body.size; i++)
+    {
+        PrintBody(ctx, node->data._program.body.arr[i]);
     }
 }
