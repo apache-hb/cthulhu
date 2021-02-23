819322b4e64133aa449c84873c120c19cb4e3484
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-02-22 22:50:34 -0500

    fix parsing issues

--- test/parse/type.cpp
+++ test/parse/type.cpp
@@ -1,9 +1,12 @@
 #include "test.hpp"
 
 int main() {
-    auto* lex = cthulhu::string(u8"int!<int>(int)");
+    auto* lex = cthulhu::string(u8"int!<int>(int) int[]");
     auto parse = cthulhu::Parser(lex);
 
     auto* node = parse.type();
     puts(node->repr().c_str());
+
+    auto* node2 = parse.type();
+    puts(node2->repr().c_str());
 }
