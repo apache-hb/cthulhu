45e761efafd0154a9c91943110c20c6337546c1c
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-02-24 19:12:16 -0500

    reorg files

---
+++ test/parse/unit.cpp
@@ -0,0 +1,25 @@
+#include "test.hpp"
+
+int main() {
+    auto* lex = cthulhu::string(u8R"(
+        using std::mem(stack);
+        using std::atomic(...);
+
+        using atomic_unit = atomic!<uint>;
+
+        let counter = atomic_uint(0);
+
+        let str = String("hello world");
+        let len = str.length();
+
+        let slice: span!<char> = str.slice(0, 5);
+    )");
+    auto parse = cthulhu::Parser(lex);
+
+    cthulhu::Printer printer;
+
+    auto* unit = parse.unit();
+    unit->visit(&printer);
+
+    puts(printer.buffer.c_str());
+}
