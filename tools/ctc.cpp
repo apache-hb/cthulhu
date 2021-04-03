38215b59630b4c89fe727482da931ac3a9c27d58
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-03 19:27:51 -0400

    cyclic imports are safe now

--- tools/ctc.cpp
+++ tools/ctc.cpp
@@ -1,43 +1,22 @@
 #include <cthulhu.h>
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 #include <fmt/core.h>
 
-using Paths = std::vector<fs::path>;
+using paths = std::vector<fs::path>;
 
 struct DefaultHandles : cthulhu::Handles {
     virtual ~DefaultHandles() { }
 
-    DefaultHandles(Paths dirs) : dirs(dirs) { }
-
-    Paths dirs;
-
-    virtual std::optional<cthulhu::File> open(const fs::path& root, const fs::path& path) override {
-        auto read = [](const fs::path& path) {
-            auto search = fs::absolute(path);
-            std::optional<cthulhu::File> file;
-            
-            if (std::ifstream in(search); !in.fail()) {
-                file = { search, std::string(std::istreambuf_iterator<char>{in}, {}) };
-            }
-            
-            return file;
-        };
-
-        // first search all include directories
-        for (const auto& dir : dirs) {
-            if (auto result = read(dir/path); result) {
-                return result;
-            }
+    virtual std::optional<std::string> open(const fs::path& path) override {
+        std::optional<std::string> file;
+        
+        if (std::ifstream in(path); !in.fail()) {
+            file = std::string(std::istreambuf_iterator<char>{in}, {});
         }
 
-        // then try and read relative to the current file
-        if (auto result = read(root/path); result) {
-            return result;
-        }
-
-        // then try the absolute path last
-        return read(path);
+        return file;
     }
 };
 
@@ -47,17 +26,12 @@ int main(int argc, const char** argv) {
         return 1;
     }
 
-    std::vector<fs::path> includes = { 
-        fs::current_path(), // include the cwd
-        fs::current_path()/"lib" // include the standard library
-    };
-
-    DefaultHandles handles(includes);
+    DefaultHandles handles;
 
     try {
         cthulhu::init(&handles);
 
-        auto ctx = cthulhu::compile(argv[1]);
+        auto ctx = cthulhu::Context::open(argv[1]);
 
     } catch (const std::exception& error) {
         std::cerr << error.what() << std::endl;
