f9f6d0963fef2f2b04b8d6487ef4f01606b8c486
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-04 19:39:00 -0400

    remove type resolution to try again

--- cthulhu/cthulhu.cpp
+++ cthulhu/cthulhu.cpp
@@ -21,27 +21,21 @@ namespace {
 
         field   <- ident colon type
 
-        type    <- pointer / mutable / closure / ident
+        type    <- pointer / ident
         pointer <- mul type
-        mutable <- var lparen type rparen
-        closure <- lparen LIST(type) rparen arrow type
 
         ~lbrace  <- '{'
         ~rbrace  <- '}'
-        ~lparen  <- '('
-        ~rparen  <- ')'
         ~assign  <- '='
         ~semi    <- ';'
         ~colon   <- ':'
         ~mul     <- '*'
-        ~arrow   <- '->'
         ~comma   <- ','
 
         ~using   <- 'using'
         ~record  <- 'record'
-        ~var     <- 'var'
 
-        keyword <- using / record / var
+        keyword <- using / record
 
         %whitespace <- spacing
 
@@ -77,86 +71,8 @@ Context::Context(std::string source) : text(source) {
     }
 
     tree = parser.optimize_ast(tree);
-}
 
-void Context::resolve() {
-    auto decl = [&](std::shared_ptr<Ast> ast) {
-        // all fields in this type decl
-        auto fields = ast->nodes[1]->nodes;
-
-        for (auto field : fields) {
-            // the name and type of the field
-            auto fname = field->nodes[0]->token_to_string();
-            auto ftype = field->nodes[1]->token_to_string();
-
-            // defer looking up the type of the field
-            // for the second phase
-            defer(ftype);
-        }
-    };
-
-    auto alias = [&](std::shared_ptr<Ast> ast) {
-        auto type = ast->nodes[1]->token_to_string();
-        defer(type);
-    };
-
-    auto parse = [&](std::shared_ptr<Ast> ast) {
-        // the name of this type decl
-        auto name = ast->nodes[0]->token_to_string();
-
-        if (ast->tag == "using"_) {
-            alias(ast);
-        } else {
-            decl(ast);  
-        }
-
-        // define this decl for the second phase
-        define(name);
-    };
-
-    // add all decls to the symbol table
-    for (auto node : tree->nodes) {
-        parse(node);
-    }
-
-    for (const auto& symbol : symbols) {
-        if (symbol.type == SymbolType::DEFER) {
-            throw std::runtime_error(fmt::format("failed to resolve symbol `{}`", symbol.name));
-        }
-    }
-}
-
-void Context::define(const std::string& name) {
-    // if the symbol has already been deferred then replace it
-    for (auto& symbol : symbols) {
-        if (symbol.name == name) {
-            if (symbol.type == SymbolType::DEFINE) {
-                throw std::runtime_error(fmt::format("symbol `{}` was already defined", name));
-            }
-            
-            symbol.type = SymbolType::DEFINE;
-            return;
-        }
-    }
-
-    // otherwise define this symbol
-    symbols.push_back({ SymbolType::DEFINE, name });
-}
-
-void Context::defer(const std::string& name) {
-    if (!lookup(name)) {
-        symbols.push_back({ SymbolType::DEFER, name });
-    }
-}
-
-bool Context::lookup(const std::string& name) {
-    for (const auto& symbol : symbols) {
-        if (symbol.name == name) {
-            return symbol.type == SymbolType::DEFINE;
-        }
-    }
-
-    return false;
+    std::cout << ast_to_s(tree) << std::endl;
 }
 
 }
--- cthulhu/cthulhu.h
+++ cthulhu/cthulhu.h
@@ -17,44 +17,15 @@ namespace cthulhu {
     // init the global compiler state
     void init();
 
-    enum struct SymbolType {
-        DEFER, // order independent lookup
-        DEFINE, // defined symbol
-    };
-
-    struct Symbol {
-        SymbolType type;
-        std::string name;
-    };
-
     struct Context {
         // create a new compilation unit
         Context(std::string source);
 
-        // resolve names
-        void resolve();
-
     private:
-        // define a symbol
-        void define(const std::string& name);
-
-        // defer a symbol
-        void defer(const std::string& name);
-
-        // check if a symbol is already defined
-        bool lookup(const std::string& name);
-
         // the source text of this compilation unit
         std::string text;
 
         // the ast from the source code
         std::shared_ptr<peg::Ast> tree;
-
-        // all symbols declared in this context
-        // start off with just builtin types
-        std::vector<Symbol> symbols = {
-            { SymbolType::DEFINE, "int" },
-            { SymbolType::DEFINE, "void" }
-        };
     };
 }
