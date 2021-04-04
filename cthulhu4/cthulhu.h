ad27ce1c21d9f1b3f0bccb60b11eada1f181a938
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-03 23:18:35 -0400

    now handle imports with items
    add aliases back tomorrow. then check for duplicate imports and clashing symbols

--- cthulhu4/cthulhu.h
+++ cthulhu4/cthulhu.h
@@ -12,11 +12,13 @@
 namespace fs = std::filesystem;
 
 namespace cthulhu {
+    struct Context;
+
     struct Handles {
         virtual ~Handles() { }
 
         // open a file and read in its source text
-        // return std::nullopt if file doesnt exist
+        // return std::nullopt if file doesn't exist
         // @param path: the path of the file to try and open
         virtual std::optional<std::string> open(const fs::path& path) = 0;
     };
@@ -24,18 +26,30 @@ namespace cthulhu {
     // init the global compiler state
     void init(Handles* handles);
 
+    struct Include {
+        // the name the context was imported with
+        std::vector<std::string> name;
+
+        // all symbols exposed from this include
+        std::optional<std::vector<std::string>> items;
+
+        // the context itself
+        std::shared_ptr<Context> context;
+    };
+
     struct Context : std::enable_shared_from_this<Context> {
         static std::shared_ptr<Context> open(const fs::path& path, const fs::path& cwd = fs::path());
 
         // create a new compilation unit
-        Context(fs::path path, std::string source);
+        Context(fs::path name, std::string source);
 
     private:
-        void process();
-        void include(std::shared_ptr<peg::Ast> ast);
+        // process just the include decls
+        void includes();
+        void include(const fs::path& path, const std::optional<std::vector<std::string>>& items);
 
         // the name of this compilation unit
-        fs::path path;
+        fs::path name;
 
         // the source text of this compilation unit
         std::string text;
@@ -43,5 +57,7 @@ namespace cthulhu {
         // the ast from the source code
         std::shared_ptr<peg::Ast> tree;
 
+        // all submodules this module directly depends on
+        std::vector<Include> submodules;
     };
 }
