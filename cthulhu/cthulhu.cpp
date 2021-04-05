f7df18a1f9a572b9e5f11e63097361a4318dde74
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-04 22:42:50 -0400

    full working name resolution
    and recursive type detection, all edge cases should be solved

--- cthulhu/cthulhu.cpp
+++ cthulhu/cthulhu.cpp
@@ -1,9 +1,6 @@
 #include "cthulhu.h"
 
-#include <iostream>
-#include <fstream>
-
-#include <fmt/format.h>
+#include <fmt/core.h>
 
 namespace cthulhu {
 
@@ -22,8 +19,9 @@ namespace {
         field   <- ident colon type
 
         type    <- pointer / ident
-        pointer <- mul type
+        pointer <- mul type { no_ast_opt }
 
+        # operators
         ~lbrace  <- '{'
         ~rbrace  <- '}'
         ~assign  <- '='
@@ -32,10 +30,14 @@ namespace {
         ~mul     <- '*'
         ~comma   <- ','
 
+        # keywords
         ~using   <- 'using'
         ~record  <- 'record'
 
-        keyword <- using / record
+        # reserved keywords
+        ~compile    <- 'compile'
+
+        keyword <- using / record / compile
 
         %whitespace <- spacing
 
@@ -50,6 +52,11 @@ namespace {
 
     // our global parser instance
     peg::parser parser;
+
+    template<typename T = std::runtime_error, typename... A>
+    [[noreturn]] void panic(const char* fmt, const A&... args) {
+        throw T(fmt::format(fmt, args...));
+    }
 }
 
 void init() {
@@ -58,21 +65,156 @@ void init() {
     };
 
     if (!parser.load_grammar(grammar)) {
-        throw std::runtime_error("failed to load grammar");
+        panic("failed to load grammar");
     }
 
     parser.enable_packrat_parsing();
     parser.enable_ast();
 }
 
+TypeSize SentinelType::size(Context* ctx) const {
+    return ctx->get(name)->size(ctx);
+}
+
+TypeSize PointerType::size(Context*) const {
+    return target::PTR;
+}
+
+TypeSize BuiltinType::size(Context*) const{
+    return self;
+}
+
+TypeSize AliasType::size(Context* ctx) const{
+    return ctx->enter(this, [&] {
+        return type->size(ctx);
+    });
+}
+
+TypeSize RecordType::size(Context* ctx) const {
+    return ctx->enter(this, [&] {
+        TypeSize length = 0;
+
+        for (const auto& [_, type] : fields) {
+            length += type->size(ctx);
+        }
+
+        return length;
+    });
+}
+
 Context::Context(std::string source) : text(source) {
     if (!parser.parse(text, tree)) {
-        throw std::runtime_error("failed to parse source");
+        panic("failed to parse source");
     }
 
     tree = parser.optimize_ast(tree);
 
     std::cout << ast_to_s(tree) << std::endl;
+}
+
+void Context::resolve() {
+    for (auto node : tree->nodes) {
+        switch (node->tag) {
+        case "struct"_:
+            add(record(node));
+            break;
+        case "alias"_:
+            add(alias(node));
+            break;
+        default:
+            panic("unrecognized node `{}`", node->name);
+        }
+    }
+
+    for (auto* type : types) {
+        if (!type->resolved()) {
+            panic("unresolved type `{}`", type->name);
+        }
+
+        auto size = type->size(this);
+
+        std::cout << "resolved: " << type->name << " size: " << size << std::endl;
+    }
+}
+
+RecordType* Context::record(std::shared_ptr<Ast> ast) {
+    auto name = ast->nodes[0]->token_to_string();
+
+    TypeFields fields;
+    for (auto node : ast->nodes) {
+        if (node->tag != "field"_)
+            continue;
+
+        fields.push_back(field(node));
+    }
+
+    return new RecordType(name, fields);
+}
+
+AliasType* Context::alias(std::shared_ptr<peg::Ast> ast) {
+    auto name = ast->nodes[0]->token_to_string();
+    auto other = type(ast->nodes[1]);
+
+    return new AliasType(name, other);
+}
+
+Field Context::field(std::shared_ptr<peg::Ast> ast) {
+    return { ast->nodes[0]->token_to_string(), type(ast->nodes[1]) };
+}
+
+Type* Context::type(std::shared_ptr<peg::Ast> ast) {
+    switch (ast->tag) {
+    case "pointer"_:
+        return new PointerType(type(ast->nodes[0]));
+    case "ident"_:
+        return get(ast->token_to_string());
+    default:
+        panic("unknown type `{}`", ast->name);
+    }
+}
+
+
+
+void Context::add(NamedType* other) {
+    for (auto& type : types) {
+        if (type->name == other->name) {
+            if (type->resolved()) {
+                panic("multiple definitions of `{}`", type->name);
+            }
+            type = other;
+        }
+    }
+
+    types.push_back(other);
+}
+
+Type* Context::get(const TypeName& name) {
+    for (auto& type : types) {
+        if (type->name == name) {
+            return type;
+        }
+    }
+
+    auto* sentinel = new SentinelType(name);
+    types.push_back(sentinel);
+    return sentinel;
+}
+
+
+// recursion testing
+
+void Context::push(const NamedType* type) {
+    for (const auto& frame : stack) {
+        if (frame->name == type->name) {
+            panic("recursive type `{}` detected", type->name);
+        }
+    }
+
+    stack.push_back(type);
+}
+
+void Context::pop() {
+    stack.pop_back();
 }
 
 }
--- cthulhu/cthulhu.h
+++ cthulhu/cthulhu.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#include <string>
-#include <map>
-#include <vector>
-#include <memory>
-#include <filesystem>
-#include <optional>
-#include <unordered_set>
 #include <peglib.h>
-
-namespace fs = std::filesystem;
 
 namespace cthulhu {
     struct Context;
@@ -17,15 +8,137 @@ namespace cthulhu {
     // init the global compiler state
     void init();
 
+    using TypeName = std::string;
+    using TypeSize = size_t;
+
+    namespace target {
+        constexpr TypeSize PTR = 8;
+    }
+
+    // all types inherit from this type
+    struct Type {
+        virtual TypeSize size(Context* ctx) const = 0;
+        virtual bool resolved() const { return true; }
+    };
+
+    struct PointerType : Type {
+        Type* type;
+
+        PointerType(Type* type)
+            : type(type)
+        { }
+
+        virtual TypeSize size(Context* ctx) const override;
+    };
+
+    struct NamedType : Type {
+        TypeName name;
+
+        NamedType(TypeName name) : name(name) { }
+    };
+
+    // a type that has yet to be resolved
+    struct SentinelType : NamedType { 
+        SentinelType(TypeName name)
+            : NamedType(name)
+        { }
+
+        virtual TypeSize size(Context* ctx) const override;
+        virtual bool resolved() const override { return false; }
+    };
+
+    // a builtin type such as int or void
+    struct BuiltinType : NamedType { 
+        TypeSize self;
+
+        BuiltinType(TypeName name, TypeSize size)
+            : NamedType(name)
+            , self(size)
+        { }
+
+        virtual TypeSize size(Context* ctx) const override;
+    };
+
+    // an alias of one type to another type
+    struct AliasType : NamedType {
+        Type* type;
+
+        AliasType(TypeName name, Type* type)
+            : NamedType(name)
+            , type(type)
+        { }
+
+        virtual TypeSize size(Context* ctx) const override;
+    };
+
+    struct Field {
+        std::string name;
+        Type* type;
+    };
+
+    using TypeFields = std::vector<Field>;
+
+    // a record composed of other types
+    struct RecordType : NamedType { 
+        TypeFields fields;
+
+        RecordType(TypeName name, TypeFields fields)
+            : NamedType(name)
+            , fields(fields)
+        { }
+
+        virtual TypeSize size(Context* ctx) const override;
+    };
+
     struct Context {
         // create a new compilation unit
         Context(std::string source);
 
-    private:
         // the source text of this compilation unit
         std::string text;
-
         // the ast from the source code
         std::shared_ptr<peg::Ast> tree;
+
+
+
+        // resolve all types in the current compilation unit
+        // also performs checks for recursion
+        void resolve();
+        RecordType* record(std::shared_ptr<peg::Ast> ast);
+        AliasType* alias(std::shared_ptr<peg::Ast> ast);
+
+        Field field(std::shared_ptr<peg::Ast> ast);
+        Type* type(std::shared_ptr<peg::Ast> ast);
+
+
+        // register a named type
+        // if a sentinal type with the same name is found
+        // then replace that type
+        void add(NamedType* other);
+
+        // find a type
+        Type* get(const TypeName& name);
+
+        // all types in the current compilation unit
+        std::vector<NamedType*> types = {
+            new BuiltinType("int", 4),
+            new BuiltinType("void", 0)
+        };
+
+
+
+        template<typename F>
+        TypeSize enter(const NamedType* type, F&& func) {
+            push(type);
+            TypeSize size = func();
+            pop();
+            return size;
+        }
+
+        void push(const NamedType* type);
+        void pop();
+
+        // used for detecting recursive types
+        std::vector<const NamedType*> stack;
     };
 }
