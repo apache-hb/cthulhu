136e02d0c8e469c362d8819f4cfd9a981211cf4b
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-06 00:34:14 -0400

    reorg some files

--- cthulhu/cthulhu.h
+++ cthulhu/cthulhu.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <peglib.h>
+#include <fmt/core.h>
 
 namespace cthulhu {
+    template<typename T = std::runtime_error, typename... A>
+    [[noreturn]] void panic(const char* fmt, const A&... args) {
+        throw T(fmt::format(fmt, args...));
+    }
+
     struct Context;
     struct NamedType;
 
