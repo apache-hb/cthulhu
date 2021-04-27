5c214d9d36e4ef641b856f3f1670cc5e3c053842
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-27 19:12:57 -0400

    add meson

--- main.cpp
+++ main.cpp
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int emit(FILE *out, char *source) {
+int compile(FILE *out, char *source) {
+    int line = 0;
+    int col = 0;
     char c;
 
     /* emit prelude */
     fprintf(out,
-        "  .globl main\n"
-        "main:\n"
+        ".globl _start\n"
+        ".section .text\n"
+        "_start:\n"
     );
 
-    fprintf(out, "  mov $%ld, %%rax\n", strtol(source, &source, 10));
+    fprintf(out, "  mov $%ld, %%rdi\n", strtol(source, &source, 10));
 
     while ((c = *source++)) {
+        line++;
         switch (c) {
         case '+':
-            fprintf(out, "  add $%ld, %%rax\n", strtol(source, &source, 10));
+            fprintf(out, "  add $%ld, %%rdi\n", strtol(source, &source, 10));
             break;
         case '-':
-            fprintf(out, "  sub $%ld, %%rax\n", strtol(source, &source, 10));
+            fprintf(out, "  sub $%ld, %%rdi\n", strtol(source, &source, 10));
             break;
-        case ' ': case '\t': case '\r': case '\n':
+        case ' ': case '\t': case '\r':
+            /* skip whitespace */
             continue;
-            
+        case '\n':
+            line = 0;
+            col++;
+            continue;
         default:
-            fprintf(stderr, "unexpected character `%c`\n", c);
+            fprintf(stderr, "unexpected character `%c` at %d:%d \n", c, line, col);
             return 1;
         }
     }
 
-    fprintf(out, "  ret\n");
+    fprintf(out, 
+        "  mov $60, %%rax\n"
+        "  syscall\n"
+    );
 
     return 0;
 }
@@ -39,6 +50,7 @@ int main(int argc, char **argv) {
     const char *name = "stdout";
     const char *self = argv[0];
     char *source;
+    int result = 0;
 
     if (argc < 2) {
         fprintf(stderr, 
@@ -62,5 +74,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    return emit(output, source);
+    result = compile(output, source);
+
+    if (result != 0) {
+        if (output != stdout) {
+            fclose(output);
+            remove(name);
+        }
+    }
+
+    return result;
 }
