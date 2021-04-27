78053c34549eb053ba832d7f7ddfe5bd46dc4638
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-27 16:11:53 -0400

    handle spaces in source

--- main.c
+++ main.c
@@ -1,35 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int emit(FILE *out, char *source) {
+    char c;
+
+    /* emit prelude */
+    fprintf(out,
+        "  .globl main\n"
+        "main:\n"
+    );
+
+    fprintf(out, "  mov $%ld, %%rax\n", strtol(source, &source, 10));
+
+    while ((c = *source++)) {
+        switch (c) {
+        case '+':
+            fprintf(out, "  add $%ld, %%rax\n", strtol(source, &source, 10));
+            break;
+        case '-':
+            fprintf(out, "  sub $%ld, %%rax\n", strtol(source, &source, 10));
+            break;
+        case ' ': case '\t': case '\r': case '\n':
+            continue;
+            
+        default:
+            fprintf(stderr, "unexpected character `%c`\n", c);
+            return 1;
+        }
+    }
+
+    fprintf(out, "  ret\n");
+
+    return 0;
+}
+
 int main(int argc, char **argv) {
+    FILE *output = stdout;
+    const char *name = "stdout";
+    const char *self = argv[0];
+    char *source;
+
     if (argc < 2) {
         fprintf(stderr, 
             "%s: invalid number of arguments\n"
             "   %s <number>\n"
             "   %s <number> <file>\n", 
-            argv[0], argv[0], argv[0]
+            self, self, self
         );
         return 1;
     }
 
-    FILE* output = stdout;
+    source = argv[1];
 
     if (argc >= 3) {
+        name = argv[2];
         output = fopen(argv[2], "w");
     }
 
     if (output == NULL) {
-        fprintf(stderr, "failed to open output file\n");
+        fprintf(stderr, "failed to open output file `%s`\n", name);
         return 1;
     }
 
-    long long number = atoll(argv[1]);
-
-    fprintf(output,
-        ".globl main\n"
-        "main:\n"
-        "mov $%lld, %%rax\n"
-        "ret\n",
-        number
-    );
+    return emit(output, source);
 }
