0b26a0a93d21e561c98f67864b92e239dc3f7ef6
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-04-26 22:37:38 -0400

    add option to specify output file

--- main.c
+++ main.c
@@ -2,14 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, 
+            "%s: invalid number of arguments\n"
+            "   %s <number>\n"
+            "   %s <number> <file>\n", 
+            argv[0], argv[0], argv[0]
+        );
+        return 1;
+    }
+
+    FILE* output = stdout;
+
+    if (argc >= 3) {
+        output = fopen(argv[2], "w");
+    }
+
+    if (output == NULL) {
+        fprintf(stderr, "failed to open output file\n");
         return 1;
     }
 
     int number = atoi(argv[1]);
 
-    printf(
+    fprintf(output,
         ".globl main\n"
         "main:\n"
         "mov $%d, %%rax\n"
