79dfafb182c774a4d641186698b7ff52e23cb4fc
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-07-18 18:48:02 -0400

    Update gen-big.py

--- scripts/gen-big.py
+++ scripts/gen-big.py
@@ -1,7 +1,15 @@
 def gen_ct(name, num):
-    with open(name, 'w') as f:       
-        for i in range(0, num):       
+    with open(name, 'w') as f:
+        for i in range(0, num):
             f.write(f'def func{i}(): void {{ }}\n')
+
+def gen_c(name, num):
+    with open(name, 'w') as f:
+        for i in range(0, num):
+            f.write(f'void func{i}() {{ }}\n')
 
 gen_ct('big-1m.ct', 1000000)
 gen_ct('big-10k.ct', 10000)
+
+gen_c('big-1m.c', 1000000)
+gen_c('big-10k.c', 10000)
