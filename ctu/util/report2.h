75f2428be76b4eb52a7d065a879c014e9440df8c
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2021-09-08 14:53:04 -0400

    Revert "more bits"
    This reverts commit 146d7177001911fb9c467805e22c0d6ca8745288.

--- ctu/util/report2.h
+++ ctu/util/report2.h
@@ -30,15 +30,16 @@ typedef struct {
     message_t *messages;
 } reports_t;
 
-reports_t *begin_report(size_t limit);
+reports_t begin_report(size_t limit);
 int end_report(reports_t *reports, const char *name);
 
 message_t *assertf(reports_t *reports, const char *fmt, ...);
 #define ASSERTF(ctx, expr, ...) ((!(expr)) ? assertf(reports, __VA_ARGS__)) : NULL
 
-message_t *reportf(reports_t *reports, const node_t *node, const char *fmt, ...);
-message_t *reportv(reports_t *reports, const node_t *node, const char *fmt, va_list args);
+message_t *report();
+message_t *reportf(const char *fmt, ...);
+message_t *reportv(const char *fmt, va_list args);
 
-void report_appendf(message_t *message, const node_t *node, const char *fmt, ...);
-void report_appendv(message_t *message, const node_t *node, const char *fmt, va_list args);
+void report_appendf(message_t *message, const char *fmt, ...);
+void report_appendv(message_t *message, const char *fmt, va_list args);
 void report_note(message_t *message, const char *fmt, ...);
