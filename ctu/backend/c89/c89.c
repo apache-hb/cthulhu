7764f0a24061240e74f313697b34b2d43675dd09
Elliot <35050275+Apache-HB@users.noreply.github.com>
2021-09-22 09:30:30 -0700

    Update c89.c

--- ctu/backend/c89/c89.c
+++ ctu/backend/c89/c89.c
@@ -3,9 +3,19 @@
 #include "ctu/util/str.h"
 
 typedef struct {
-    stream_t *globals;
-    stream_t *functions;
+    reports_t *reports;
+    module_t *mod;
+
+    vector_t *globals;
 } context_t;
+
+static context_t *init_c89_context(reports_t *reports, module_t *mod) {
+    context_t *ctx = NEW(context_t);
+    ctx->reports = reports;
+    ctx->mod = mod;
+    ctx->globals = vector_new(vector_len(mod->vars));
+    return ctx;
+}
 
 bool c89_build(reports_t *reports, module_t *mod, const char *path) {
     assert2(reports, "c89 unimplemented %p %s", mod, path);
