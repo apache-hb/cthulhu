d2cbdcfc587b0715bbb5e2ed8afd26c8a50457e0
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2022-03-10 02:02:10 -0500

    add more memory usage info

--- tools/tune-map.c
+++ tools/tune-map.c
@@ -147,17 +147,37 @@ static ns_t avg_runs(size_t len, size_t size) {
 
 #define PRIMES (sizeof(MERSENNE) / sizeof(size_t))
 
-static size_t time_primes(size_t len) {
-    ns_t times[PRIMES];
+static size_t RUNS[] = {
+    1, 10, 100, 1000, 10000, 100000, 500000, 1000000
+};
+
+static counters_t MAX_COUNTERS = {
+    .mallocs = SIZE_MAX,
+    .reallocs = SIZE_MAX,
+    .frees = SIZE_MAX,
+    
+    .current = SIZE_MAX,
+    .peak = SIZE_MAX
+};
+
+#define TOTAL_RUNS (sizeof(RUNS) / sizeof(size_t))
+
+static counters_t counters[TOTAL_RUNS][PRIMES];
+static ns_t times[TOTAL_RUNS][PRIMES];
+static size_t winners[TOTAL_RUNS];
+
+static void time_primes(size_t self, size_t len) {
+    size_t i = 0;
     size_t used = 0;
 
     ns_t last = SIZE_MAX / 2;
 
-    for (size_t i = 0; i < PRIMES; i++) {
+    for (; i < PRIMES; i++) {
         size_t prime = MERSENNE[i];
         if (prime < (len / 100)) {
             printf("skipping size=%zu for len=%zu\n", prime, len);
-            times[used++] = SIZE_MAX;
+            times[self][used++] = SIZE_MAX;
+            counters[self][i] = MAX_COUNTERS;
             continue;
         }
 
@@ -165,13 +185,15 @@ static size_t time_primes(size_t len) {
         
         if (avg == 0) {
             printf("\naborting size=%zu for len=%zu due to long run\n", prime, len);
-            times[used++] = SIZE_MAX;
+            times[self][used++] = SIZE_MAX;
+            counters[self][i] = MAX_COUNTERS;
             continue;
         }
 
         diff(len, prime, avg);
         
-        times[used++] = avg;
+        times[self][used++] = avg;
+        counters[self][i] = reset_counters();
 
         if ((last * 2) < avg) {
             printf("passed reasonable end\n");
@@ -181,20 +203,99 @@ static size_t time_primes(size_t len) {
         last = avg;
     }
 
-    size_t idx = min_index(times, used);
+    for (; i < PRIMES; i++) {
+        counters[self][i] = MAX_COUNTERS;
+    }
+
+    size_t idx = min_index(times[self], used);
     size_t prime = MERSENNE[idx];
 
-    return prime;
+    winners[self] = prime;
 }
 
-static size_t RUNS[] = {
-    1, 10, 100, 1000, 10000, 100000, 500000, 1000000
-};
+static counters_t avg_counters(size_t i) {
+    counters_t out = { 0 };
+    size_t n = 0;
 
-#define TOTAL_RUNS (sizeof(RUNS) / sizeof(size_t))
+    for (size_t j = 0; j < PRIMES; j++) {
+        counters_t c = counters[i][j];
+        if (c.mallocs == SIZE_MAX) { continue; }
 
-static counters_t counters[TOTAL_RUNS];
-static size_t winners[TOTAL_RUNS];
+        out.mallocs += c.mallocs;
+        out.reallocs += c.reallocs;
+        out.frees += c.frees;
+        out.current += c.current;
+        out.peak += c.peak;
+
+        n += 1;
+    }
+
+    out.mallocs /= n;
+    out.reallocs /= n;
+    out.frees /= n;
+    out.current /= n;
+    out.peak /= n;
+
+    return out;
+}
+
+typedef struct {
+    counters_t most;
+    size_t most_idx;
+
+    counters_t least;
+    size_t least_idx;
+} count_pair_t;
+
+static count_pair_t most_allocs(size_t i) {
+    count_pair_t out = { 
+        .least = {
+            .mallocs = SIZE_MAX,
+        }
+    };
+
+    for (size_t j = 0; j < PRIMES; j++) {
+        counters_t c = counters[i][j];
+        if (c.mallocs == SIZE_MAX) { continue; }
+
+        if (out.most.mallocs < c.mallocs) {
+            out.most = c;
+            out.most_idx = j;
+        }
+
+        if (out.least.mallocs > c.mallocs) {
+            out.least = c;
+            out.least_idx = j;
+        }
+    }
+
+    return out;
+}
+
+static count_pair_t most_memory(size_t i) {
+    count_pair_t out = { 
+        .least = {
+            .peak = SIZE_MAX,
+        }
+    };
+
+    for (size_t j = 0; j < PRIMES; j++) {
+        counters_t c = counters[i][j];
+        if (c.peak == SIZE_MAX) { continue; }
+
+        if (out.most.peak < c.peak) {
+            out.most = c;
+            out.most_idx = j;
+        }
+
+        if (out.least.peak > c.peak) {
+            out.least = c;
+            out.least_idx = j;
+        }
+    }
+
+    return out;
+}
 
 int main(void) {
     STAGE("init", {
@@ -205,25 +306,54 @@ int main(void) {
 
     for (size_t i = 0; i < TOTAL_RUNS; i++) {
         size_t len = RUNS[i];
-        winners[i] = time_primes(len);
-        counters[i] = reset_counters();
+        time_primes(i, len);
     }
 
     for (size_t i = 0; i < TOTAL_RUNS; i++) {
         size_t len = RUNS[i];
         size_t prime = winners[i];
-        printf("===\n=== winner for items=%zu is size=%zu\n===\n", len, prime);
+        counters_t avg = avg_counters(i);
 
-        counters_t mem = counters[i];
+        count_pair_t malls = most_allocs(i);
+        count_pair_t peaks = most_memory(i);
+
+        counters_t peak_malls = malls.most;
+        counters_t least_malls = malls.least;
+
+        counters_t peak_peaks = peaks.most;
+        counters_t least_peaks = peaks.least;
+
+        ns_t least_malls_time = times[i][malls.least_idx];
+        ns_t least_peaks_time = times[i][peaks.least_idx];
+        ns_t best_time = times[i][min_index(times[i], PRIMES)];
+
         printf(
-            "memory stats for size %zu:\n"
-            "\tmallocs   : %zu\n"
-            "\treallocs  : %zu\n"
-            "\tfrees     : %zu\n"
-            "\tpeak usage: %zu\n",
-            len,
-            mem.mallocs, mem.reallocs, mem.frees, 
-            mem.peak
+            "stats for size: %zu\n"
+            "performance stats:\n"
+            "\tbest prime: %zu\n"
+            "\tfastest: %lu.%06lus\n"
+            "\ttime for least memory used: %lu.%06lus\n"
+            "\ttime for least mallocs: %lu.%06lus\n"
+            "memory stats:\n"
+            "\tsize that caused the most mallocs: %zu, size that caused the least: %zu\n"
+            "\tsize that had the most memory allocated: %zu, size that had the least: %zu\n"
+            "\t          | avg             | mallocs (min, max)              | peak (min, max)\n"
+            "\tmallocs   : %-15zu | %-15zu %-15zu | %-15zu %-15zu\n"
+            "\treallocs  : %-15zu | %-15zu %-15zu | %-15zu %-15zu\n"
+            "\tfrees     : %-15zu | %-15zu %-15zu | %-15zu %-15zu\n"
+            "\tpeak usage: %-15zu | %-15zu %-15zu | %-15zu %-15zu\n"
+            ,
+            len, 
+            prime,
+            (unsigned long)(best_time / 1000000000), (unsigned long)(best_time % 1000000000),
+            (unsigned long)(least_peaks_time / 1000000000), (unsigned long)(least_peaks_time % 1000000000),
+            (unsigned long)(least_malls_time / 1000000000), (unsigned long)(least_malls_time % 1000000000),
+            MERSENNE[malls.most_idx], MERSENNE[malls.least_idx],
+            MERSENNE[peaks.most_idx], MERSENNE[peaks.least_idx],
+            avg.mallocs, peak_malls.mallocs, least_malls.mallocs, peak_peaks.mallocs, least_peaks.mallocs,
+            avg.reallocs, peak_malls.reallocs, least_malls.reallocs, peak_peaks.reallocs, least_peaks.reallocs,
+            avg.frees, peak_malls.frees, least_malls.frees, peak_peaks.frees, least_peaks.frees,
+            avg.peak, peak_malls.peak, least_malls.peak, peak_peaks.peak, least_peaks.peak
         );
     }
 }
