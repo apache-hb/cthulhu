d8ea255ef2df802026f0b1692bdd4473edb9cf5c
Elliot <35050275+Apache-HB@users.noreply.github.com>
2022-04-12 04:05:27 -0400

    Update sema.c

--- driver/pl0/sema.c
+++ driver/pl0/sema.c
@@ -380,7 +380,6 @@ hlir_t *pl0_sema(reports_t *reports, void *node) {
     vector_push(&procs, PRINT);
 
     const char *modname = root->mod == NULL ? ctu_filename(root->node->scan->path) : root->mod;
-
     hlir_t *mod = hlir_module(root->node, modname, vector_of(0), vector_join(consts, globals), procs);
     
     sema_delete(sema);
