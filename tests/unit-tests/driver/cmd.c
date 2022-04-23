8fbe4a335c4d30b06de7ff712b6b7e5e68fbdb3d
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2022-04-23 02:39:10 -0400

    Update cmd.c

--- tests/unit-tests/driver/cmd.c
+++ tests/unit-tests/driver/cmd.c
@@ -4,14 +4,29 @@
 
 TEST(test_cmd_files, {
     reports_t *reports = begin_reports();
-    commands_t commands;
-    const char *argv[] = { "test-harness", "hello.test" };
-    int argc = 2;
-    int result = parse_commandline(reports, &commands, argc, argv);
-    
+    commands_t commands = { 0 };
+    int result;
+    {
+        const char *argv[] = { "test-harness", "hello.test" };
+        int argc = 2;
+        result = parse_commandline(reports, &commands, argc, argv);
+    }
+
     SHOULD_PASS("parse one file status", result == 0);
     SHOULD_PASS("has one file", vector_len(commands.sources) == 1);
-    SHOULD_PASS("has correct file", str_equal(vector_get(commands.sources, 0), "hello.test") );
+    SHOULD_PASS("has correct file", str_equal(vector_get(commands.sources, 0), "hello.test"));
+
+    {
+        const char *argv[] = { "test-harness", "hello.test", "world.test", "foo.bar" };
+        int argc = 4;
+        result = parse_commandline(reports, &commands, argc, argv);
+    }
+
+    SHOULD_PASS("parse two files status", result == 0);
+    SHOULD_PASS("has 3 files", vector_len(commands.sources) == 3);
+    SHOULD_PASS("has correct first file", str_equal(vector_get(commands.sources, 0), "hello.test"));
+    SHOULD_PASS("has correct second file", str_equal(vector_get(commands.sources, 1), "world.test"));
+    SHOULD_PASS("has correct third file", str_equal(vector_get(commands.sources, 2), "foo.bar"));
 })
 
 HARNESS("cmd", {
