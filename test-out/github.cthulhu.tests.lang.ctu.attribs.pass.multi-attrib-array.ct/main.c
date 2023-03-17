1d743ffeff19eafee0f8953850550d11d4752eab
Elliot <35050275+apache-hb@users.noreply.github.com>
2023-03-17 17:27:23 -0400

    oops

--- test-out/github.cthulhu.tests.lang.ctu.attribs.pass.multi-attrib-array.ct/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-#include <stddef.h>
-static signed int doThing(signed int i);
-static signed int doThing(signed int param0) {
-  entry: {
-    signed int reg0 = *param0;
-    return reg0;
-  }
-}
