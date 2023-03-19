6107e1c11d396d06f9b52305f766f87ab5528c48
Elliot <35050275+apache-hb@users.noreply.github.com>
2023-03-19 04:13:22 -0400

    frontend tool start

---
+++ tools/frontend/core.py
@@ -0,0 +1,230 @@
+from typing import Mapping, List, TextIO
+from dataclasses import dataclass
+
+@dataclass
+class LexPattern:
+    pattern: str
+    name: str
+    action: str
+    tag: str
+
+    def write_pattern(self, fd: TextIO):
+        raise NotImplementedError()
+
+    def token_name(self) -> str:
+        return ' '
+
+    def make_token_decl(self) -> str:
+        return '%token'
+
+@dataclass
+class LexIgnore(LexPattern):
+    def write_pattern(self, fd: TextIO):
+        fd.write(f'{self.pattern} ;\n')
+
+@dataclass
+class LexTransition(LexPattern):
+    state: str
+
+    def write_pattern(self, fd: TextIO):
+        fd.write(f'"{self.pattern}" {{ BEGIN({self.state}); }}\n')
+
+@dataclass
+class LexToken(LexPattern):
+    field: str
+
+    def make_action(self) -> str:
+        inner = self.action + '; ' if self.action else ''
+        return inner.replace('$type', f'yylval->{self.field}')
+    
+    def write_pattern(self, fd: TextIO):
+        action = self.make_action()
+        fd.write(f'{self.pattern} {{ {action}return {self.tag}; }}\n')
+
+    def token_name(self) -> str:
+        return f' "{self.name}"'
+
+    def make_token_decl(self) -> str:
+        field = f'<{self.field}>' if self.field else ''
+        return f'%token{field}'
+
+@dataclass
+class LexSymbol(LexPattern):
+    def write_pattern(self, fd: TextIO):
+        action = self.action + '; ' if self.action else ''
+        fd.write(f'"{self.pattern}" {{ {action}return {self.tag}; }}\n')
+
+    def token_name(self) -> str:
+        return f' "{self.pattern}"'
+
+@dataclass
+class LexState:
+    patterns: List[LexPattern]
+
+@dataclass
+class Lexer:
+    initial: str
+    states: Mapping[str, LexState]
+    union: Mapping[str, str]
+
+    def write_state(self, fd: TextIO, name: str):
+        if name == self.initial:
+            return
+    
+        fd.write(f'<{name}> ')
+
+    def declare_state(self, fd: TextIO, name: str):
+        if name == self.initial:
+            return
+
+        fd.write(f'%x {name}\n')
+
+@dataclass
+class RuleSegment:
+    name: str
+
+    def get_part(self):
+        return self.name
+
+@dataclass
+class RuleToken(RuleSegment):
+    pass
+
+@dataclass
+class RuleSymbol(RuleSegment):
+    pass
+
+class EmptyRule(RuleSegment):
+    def __init__(self):
+        super().__init__('')
+
+    def get_part(self):
+        return '%empty'
+
+@dataclass
+class RuleMatch:
+    action: str
+    parts: List[RuleSegment]
+
+    def make_rule(self) -> str:
+        rule = ' '.join([p.get_part() for p in self.parts])
+        return f'{rule} {{ {self.action} }}'
+
+@dataclass
+class GrammarRule:
+    name: str
+    result: str
+    matches: List[RuleMatch]
+
+    def write_type(self, fd: TextIO):
+        if self.result:
+            fd.write(f'%type<{self.result}> {self.name}\n')
+
+    def write_rule(self, fd: TextIO):
+        fd.write(f'{self.name} : ')
+        fd.write(' | '.join([m.make_rule() for m in self.matches]))
+        fd.write(';\n')
+
+@dataclass
+class Grammar:
+    entry: str
+    rules: List[GrammarRule]
+
+    def write_token(self, fd: TextIO, pattern: LexPattern):
+        if isinstance(pattern, (LexIgnore, LexTransition)):
+            return
+        
+        fd.write(f'{pattern.make_token_decl()} {pattern.tag}{pattern.token_name()}\n')
+
+def flex_preamble(name):
+    return f'''\
+%option extra-type="scan_t*"
+%option 8bit nodefault
+%option noyywrap noinput nounput
+%option noyyalloc noyyrealloc noyyfree
+%option reentrant bison-bridge bison-locations
+%option never-interactive batch
+%option prefix="{name}"
+
+%{{
+#include "{name}-bison.h"
+#include "report/report-ext.h"
+#include "interop/flex.h"
+%}}
+'''
+
+def bison_preamble(name):
+    return f'''\
+%define parse.error verbose
+%define api.pure full
+%lex-param {{ void *scan }}
+%parse-param {{ void *scan }} {{ scan_t *x }}
+%locations
+%expect 0
+%define api.prefix {{{name}}}
+
+%code top {{
+    #include "interop/flex.h"
+}}
+
+%{{
+int {name}lex();
+void {name}error(where_t *where, void *state, scan_t *scan, const char *msg);
+%}}
+'''
+
+@dataclass
+class Frontend:
+    name: str
+    lexer: Lexer
+    grammar: Grammar
+
+    def write_tmlang(self, fd: TextIO):
+        pass
+
+    def write_flex(self, fd: TextIO):
+        fd.write(flex_preamble(self.name))
+        fd.write('\n')
+        for name in self.lexer.states.keys():
+            self.lexer.declare_state(fd, name)
+
+        fd.write('\n%%\n\n')
+
+        for name, state in self.lexer.states.items():
+            for pattern in state.patterns:
+                self.lexer.write_state(fd, name)
+                pattern.write_pattern(fd)
+            fd.write('\n')
+
+        fd.write('. { report_unknown_character(scan_reports(yyextra), node_new(yyextra, *yylloc), yytext); }\n')
+
+        fd.write('\n%%\n')
+        fd.write(f'\nFLEX_MEMORY({self.name}alloc, {self.name}realloc, {self.name}free)\n')
+
+    def write_bison(self, fd: TextIO):
+        fd.write(bison_preamble(self.name))
+        fd.write('\n')
+        
+        fd.write('%union {\n')
+        for name, kind in self.lexer.union.items():
+            fd.write(f'    {kind} {name};\n')
+        fd.write('}\n\n')
+
+        for name, state in self.lexer.states.items():
+            for pattern in state.patterns:
+                self.grammar.write_token(fd, pattern)
+
+        for rule in self.grammar.rules:
+            rule.write_type(fd)
+
+        fd.write(f'\n%start {self.grammar.entry}\n\n')
+
+        fd.write('%%\n\n')
+
+        for rule in self.grammar.rules:
+            rule.write_rule(fd)
+
+        fd.write('\n%%\n')
+
+    def write_ast(self, fd: TextIO):
+        pass
---
+++ tools/frontend/main.py
@@ -0,0 +1,132 @@
+import sys
+import os
+from core import *
+import xml.dom.minidom as xml
+
+
+def get_text(node):
+    return ''.join([n.data for n in node.childNodes if n.nodeType == n.TEXT_NODE])
+
+# token parsing
+
+
+def parse_ignore(dom):
+    pattern = dom.getAttribute('pattern')
+    return LexIgnore(pattern, get_text(dom), None, None)
+
+
+def parse_transition(dom):
+    pattern = dom.getAttribute('pattern')
+    return LexTransition(pattern, get_text(dom), None, None, dom.getAttribute('state'))
+
+
+def parse_token(dom):
+    pattern = dom.getAttribute('pattern')
+    return LexToken(pattern,
+                    get_text(dom),
+                    dom.getAttribute('action'),
+                    dom.getAttribute('tag'),
+                    dom.getAttribute('type'))
+
+
+def parse_symbol(dom):
+    pattern = dom.getAttribute('pattern')
+    return LexSymbol(pattern, get_text(dom), dom.getAttribute('action'), dom.getAttribute('tag'))
+
+
+def parse_pattern(dom):
+    kind = dom.tagName
+    match kind:
+        case 'ignore':
+            return parse_ignore(dom)
+        case 'transition':
+            return parse_transition(dom)
+        case 'token':
+            return parse_token(dom)
+        case 'symbol':
+            return parse_symbol(dom)
+
+
+def parse_state(dom):
+    patterns = [parse_pattern(
+        node) for node in dom.childNodes if node.nodeType == node.ELEMENT_NODE]
+
+    return LexState(patterns)
+
+
+def parse_fields(dom):
+    return {get_text(node): node.getAttribute('type') for node in dom.getElementsByTagName('field')}
+
+
+def parse_tokens(dom):
+    initial = dom.getAttribute('initial')
+    states = {node.getAttribute('name'): parse_state(node)
+              for node in dom.getElementsByTagName('state')}
+    fields = parse_fields(dom)
+
+    return Lexer(initial, states, fields)
+
+# grammar parsing
+
+
+def parse_item(node):
+    kind = node.tagName
+    match kind:
+        case 'token':
+            return RuleToken(get_text(node))
+        case 'rule':
+            return RuleSymbol(get_text(node))
+        case 'empty':
+            return EmptyRule()
+
+
+def parse_match(dom):
+    action = dom.getAttribute('action')
+    pattern = [parse_item(node) for node in dom.childNodes if node.nodeType == node.ELEMENT_NODE]
+    return RuleMatch(action, pattern)
+
+def parse_rule(dom):
+    name = dom.getAttribute('name')
+    result = dom.getAttribute('type')
+    matches = [parse_match(node) for node in dom.getElementsByTagName('match')]
+
+    print(name)
+
+    return GrammarRule(name, result, matches)
+
+
+def parse_grammar(dom):
+    # getElementsByTagName turns out to be pretty broken in this case
+    entry = dom.getAttribute('entry')
+    rules = [parse_rule(node) for node in dom.childNodes if node.nodeType == node.ELEMENT_NODE and node.tagName == 'rule']
+    return Grammar(entry, rules)
+
+def main():
+    path = sys.argv[1]
+    name = sys.argv[2]
+
+    token_dom = xml.parse(f'{path}/tokens.xml')
+    tokens = parse_tokens(token_dom.getElementsByTagName('tokens')[0])
+
+    grammar_dom = xml.parse(f'{path}/grammar.xml')
+    grammar = parse_grammar(grammar_dom.getElementsByTagName('grammar')[0])
+
+    it = Frontend('pl0', tokens, grammar)
+
+    try:
+        os.mkdir(f'{name}-out')
+    except:
+        pass
+
+    with open(f'{name}-out/{name}.tmlang.json', 'w') as fd:
+        it.write_tmlang(fd)
+
+    with open(f'{name}-out/{name}.l', 'w') as fd:
+        it.write_flex(fd)
+
+    with open(f'{name}-out/{name}.y', 'w') as fd:
+        it.write_bison(fd)
+
+
+if __name__ == "__main__":
+    main()
