76c5c2db2c4aca07bc6a800db33458b0e7c32bd4
Elliot <35050275+apache-hb@users.noreply.github.com>
2024-01-20 04:20:14 -0500

    Update sema.cpp

--- src/tools/reflect/src/sema.cpp
+++ src/tools/reflect/src/sema.cpp
@@ -757,10 +757,10 @@ void Variant::emit_impl(out_t& out) const
     out.writeln("{} m_value;", under);
     out.nl();
     out.writeln("constexpr {}({} value) : m_value(value) {{ }}", get_name(), under);
-    out.writeln("constexpr {}(inner_t value) : m_value(({})value) {{ }}", get_name(), under);
     out.leave();
     out.writeln("public:");
     out.enter();
+    out.writeln("constexpr {}(inner_t value) : m_value(({})value) {{ }}", get_name(), under);
     out.writeln("using enum inner_t;");
     out.nl();
     if (m_default_case)
