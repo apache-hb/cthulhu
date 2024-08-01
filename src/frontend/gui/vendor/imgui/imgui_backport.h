44898e9f3d1d7721288fc84dc66f019b69bf6f42
Elliot <35050275+apache-hb@users.noreply.github.com>
2024-08-01 15:52:02 -0400

    remove imgui

--- src/frontend/gui/vendor/imgui/imgui_backport.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "imgui/imgui.h"
-
-namespace ImGui
-{
-    bool BeginChildFrame(ImGuiID id, const ImVec2& size, ImGuiWindowFlags extra_flags = 0);
-    void EndChildFrame();
-}
