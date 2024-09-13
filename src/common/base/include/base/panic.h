4a1026bda1af64c1b024cc04ed4028b6a6c67abc
Elliot <35050275+apache-hb@users.noreply.github.com>
2024-09-13 14:56:16 -0400

    Update panic.h

--- src/common/base/include/base/panic.h
+++ src/common/base/include/base/panic.h
@@ -106,7 +106,7 @@ CT_BASE_API CT_NORETURN ctu_vpanic(source_info_t location, STA_FORMAT_STRING con
 #elif defined(__cplusplus) && (defined(__clang__) || defined(__GNUC__))
 #   define CTASSERTF(expr, ...) \
     do { \
-        if constexpr (__builtin_constant_p(expr) && (expr)) { \
+        if (__builtin_constant_p(expr) && !(expr)) { \
             CT_UNREACHABLE(); \
         } else { \
             CTASSERTF_ALWAYS(expr, __VA_ARGS__); \
